c723d5f4444664006453184a038b3fca35ec605b
Nattaporn11 <101962761+Nattaporn11@users.noreply.github.com>
2022-05-10 09:39:26 +0700

    update firebase eye

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -9,13 +9,12 @@ import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCbawltp6yBwq8H3LVaQB6i9ewqpGSsU6c",
-  authDomain: "vue-web-app-90e99.firebaseapp.com",
-  projectId: "vue-web-app-90e99",
-  storageBucket: "vue-web-app-90e99.appspot.com",
-  messagingSenderId: "1067183826914",
-  appId: "1:1067183826914:web:6bb80fc39701e1482c3bbb",
-  measurementId: "G-2JYZ6R28EG"
+  apiKey: "AIzaSyB-rotebhxqSd71961FNM0RN9TLMzmxl4Y",
+  authDomain: "medical-system-c125e.firebaseapp.com",
+  projectId: "medical-system-c125e",
+  storageBucket: "medical-system-c125e.appspot.com",
+  messagingSenderId: "1041823010079",
+  appId: "1:1041823010079:web:fb86d5d012037cbf14742a"
 };
 
 // Initialize Firebase
