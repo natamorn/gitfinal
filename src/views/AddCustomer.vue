f44aae969437be500ef3492b54994abfa62a5281
Nattaporn11 <101962761+Nattaporn11@users.noreply.github.com>
2022-05-21 00:17:29 +0700

    edit work phone

--- src/views/AddCustomer.vue
+++ src/views/AddCustomer.vue
@@ -37,7 +37,7 @@
                 <input
                   v-model="formData.WorkPhone"
                   type="tel"
-                  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                  pattern="[0-9]{3}-[0-9]{2}-[0-9]{4}"
                   :class="[
                     'form-control',
                     formWarning.WorkPhone ? '' : 'is-invalid',
